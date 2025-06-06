b0c6000f8c7f14c719b7c7ad013efeebd70d9f28
nahel <nahelawad123@gmail.com>
2025-06-06 05:01:31 +0100

    Added some new components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono,Bebas_Neue} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +11,15 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const Bebas_Font=Bebas_Neue({
+  subsets:['latin'],
+  weight:['400'],
+  variable:"--font-bebas",
+});
+
+
+
 
 export const metadata: Metadata = {
   title: "AHLY STORE",
@@ -25,7 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${Bebas_Font.variable} antialiased background `}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h2 className="text-center mt-90">
-      Al Ahly Club of the Century Store Stay Tuned
-    </h2>
+    <>
+
+   <div className="flex flex-col items-center justify-center min-h-screen text-center p-4">
+      <img className="w-40 h-auto mb-6" src="/Ahly.png" alt="Ahly Logo" />
+
+      <h2 className="font-bebas text-[36px] mb-4">
+        Al Ahly <br />
+        The Club of the Century Store Stay Tuned
+      </h2>
+
+      <Button className="font-bebas mt-12 text-3xl bg-black pt-6 pb-6 text-center transform hover:scale-105">
+        Join the Waitlist
+      </Button>
+    </div>
+    
+    </>
+    
   );
 }
