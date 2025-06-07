716112dd3e2468bc137da32351bd0fc3da840896
nahel <nahelawad123@gmail.com>
2025-06-07 17:01:49 +0100

    added clerk to my application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono,Bebas_Neue} from "next/font/google";
 import "./globals.css";
 
+import {ClerkProvider} from "@clerk/nextjs"
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -32,12 +34,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+      <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${Bebas_Font.variable} antialiased background `}
       >
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
@@ -6,6 +7,8 @@ export default function Home() {
     <>
 
    <div className="flex flex-col items-center justify-center min-h-screen text-center p-4">
+
+    <UserButton/>
       <img className="w-40 h-auto mb-6" src="/Ahly.png" alt="Ahly Logo" />
 
       <h2 className="font-bebas text-[36px] mb-4">
